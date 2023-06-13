1ae6cd20eb07d6fc7267861882d3466ce75a35c5
reddeew <104558072+reddeew@users.noreply.github.com>
2023-06-13 09:43:59 +0100

    Update and rename datalake_project3.py to etl.py

--- etl.py
+++ etl.py
@@ -28,22 +28,23 @@ def process_song_data(spark, input_data, output_data):
 	
     song_data = input_data + 'song_data/*/*/*/*.json'
 
-   
+   # reading song data
     df = spark.read.json(song_data)
 
-   
+   #selecting columns for song table 
     songs_table =df.select('song_id','title','artist_id','year','duration').dropDuplicates()
 
- 
+   # writting song table to s3
     songs_table.write.partitionBy('year','artist_id').parquet(os.path.join(output_data,'songs/songs.parquet'),'overwrite')
 
-   
+   # extracting artist table columns
     artists_table=df.select('artist_id','artist_name','artist_location','artist_latitude','artist_longitude')\
     .withColumnRenamed('artist_name', 'name') \
     .withColumnRenamed('artist_location','location') \
     .withColumnRenamed('aritist_latitude','latitude') \
     .withColumnRenamed('artist_longtitude','longtitude') \
     .dropDuplicates()
+  # writting artist table to s3
     artists_table.write.parquet(os.path.join(output_data,'artists/artists.parquet'),'overwrite')
 
 def process_log_data(spark, input_data, output_data):
@@ -56,28 +57,28 @@ def process_log_data(spark, input_data, output_data):
  
     log_data = input_data + 'log_data/*/*/*.json'
 
- 
+   # reading log data 
     df = spark.read.json(log_data)
     
-  
+    # filter table based on actions
     actions_df = df.filter(df.page=='NextSong') \
         .select('ts','userId','level','song','artist','sessionId','location','userAgent')
 
-    
+    # select user table columns  
     users_table = df.select('userid', 'firstName', 'lastName', 'gender', 'level').dropDuplicates()
-
+     # create ser table view 
     users_table.createOrReplaceTempView('users')
- 
+     # write data to user parquet file
     artists_table=users_table.write.parquet(os.path.join(output_data,'users/users.parquet'),'overwrite')
-
+    # chagne timestamp
     get_timestamp = udf(lambda x: str(int(int(x)/1000)))
     actions_df = actions_df.withColumn('timestamp,get_timestamp(actions_df.ts)')
     
-    
+    # change datatime
     get_datetime = udf(lambda x : str(datetime.fromtimestamp(int(x)/1000)))
     actions_df = actions_df.withColumn('datetime ,get_datetime(actions_df.ts)')
     
-  
+    # select time table column 
     time_table = actions_df.select('datetime') \
                  .withColumn('start_time' , actions_df.datetime) \
                  .withColumn('hour', hour('datetime')) \
@@ -89,7 +90,7 @@ def process_log_data(spark, input_data, output_data):
                  .dropDuplicates()
                  
     
-  
+   # write data to table columns
     time_table.write.partitionBy('year','month') \
                  .parquet(os.path.json(output_data,'time/time.parquet'), 'overwrite')
 				
@@ -100,7 +101,7 @@ def process_log_data(spark, input_data, output_data):
     actions_df= actions_df.alias('log_df')
     song_df = song_df.alias('song_df')
     joined_df = actions_df.join(song_df,col('log_df.artist') ==col('song_df.artist_name'),'inner')
-    
+    # select songplay table columns
     songplays_table = joined_df.select(col('log_df.datetime').alias('start_time'),
                                         col('log_df.userId').alias('user_Id'),
                                         col('log_df.level').alias('level'),
@@ -114,7 +115,7 @@ def process_log_data(spark, input_data, output_data):
                                         .withColumn('songplay_id',monotonically_increasing_id())
     songplays_table.createOrReplaceTempView('songplays')
 
-    
+    # write data to songplays parquet file
     time_table = time_table.alias('timetable')
     songplays_table.write.partitionBy('year','month').parquet(os.path.join(output_data,'songplays/songplays.parquet'),'overwrite')
 
